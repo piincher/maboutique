a8713e4bb4fc8d941353540143b9295be86edee2
Ibrahim Kouma <ikouma269@gmail.com>
2021-04-27 22:05:37 +0800

    Homepage and directory component

---
+++ src/pages/homepage/HomePage.component.jsx
@@ -0,0 +1,10 @@
+import './HomePage.component';
+import Directory from '../../components/directory/Directory.component';
+const HomePage = () => {
+	return (
+		<div className="homepage">
+			<Directory />
+		</div>
+	);
+};
+export default HomePage;
