1c066fe4f43e74ef9641fcb3e4a8456d29722703
Ibrahim Kouma <ikouma269@gmail.com>
2021-05-15 21:10:31 +0800

    update our app to redirect to the home if user is sign in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import './App.css';
 
@@ -12,14 +12,6 @@ import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
 
 class App extends React.Component {
-	constructor() {
-		super();
-
-		this.state = {
-			currentUser: null
-		};
-	}
-
 	unsubscribeFromAuth = null;
 
 	componentDidMount() {
@@ -50,13 +42,21 @@ class App extends React.Component {
 				<Switch>
 					<Route exact path="/" component={HomePage} />
 					<Route path="/shop" component={ShopPage} />
-					<Route path="/signin" component={SignInAndSignUpPage} />
+					<Route
+						exact
+						path="/signin"
+						component={SignInAndSignUpPage}
+						render={() => (this.props.currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />)}
+					/>
 				</Switch>
 			</div>
 		);
 	}
 }
+const mapStateToProps = ({ user }) => ({
+	currentUser: user.currentUser
+});
 const mapStateToDispatch = (dispatch) => ({
 	setCurrentUser: (user) => dispatch(setCurrentUser(user))
 });
-export default connect(null, mapStateToDispatch)(App);
+export default connect(mapStateToProps, mapStateToDispatch)(App);
