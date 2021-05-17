ade1c2bbe3dfd5c5abfe0268ac68489e401595fe
Ibrahim Kouma <ikouma269@gmail.com>
2021-05-17 21:26:08 +0800

    cart-icon component

--- src/components/header/Header.component.jsx
+++ src/components/header/Header.component.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
@@ -29,6 +30,8 @@ const Header = ({ currentUser }) => (
 					SIGN IN
 				</Link>
 			)}
+
+			<CartIcon />
 		</div>
 	</div>
 );
