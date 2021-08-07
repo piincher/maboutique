eb5e8f08bfdec8ef1d7b231dabf64bf7598419d3
Ibrahim Kouma <ikouma269@gmail.com>
2021-08-08 03:02:40 +0800

    created chekout page 2

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -7,3 +7,7 @@ export const selectCartItems = createSelector([ selectCart ], (cart) => cart.car
 export const selectCartItemsCount = createSelector([ selectCartItems ], (cartItems) =>
 	cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0)
 );
+
+export const selectCartTotal = createSelector([ selectCartItems ], (cartItems) =>
+	cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity * cartItem.price, 0)
+);
