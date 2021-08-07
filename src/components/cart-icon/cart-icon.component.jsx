a76f89458407d78afe44bf576260384c55ad02e0
Ibrahim Kouma <ikouma269@gmail.com>
2021-08-08 01:37:48 +0800

    update our app with memoize version

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import { connect } from 'react-redux';
+
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shoppingIcon.svg';
 
 import './cart-icon.styles.scss';
 
-const CartIcon = ({ toggleCartHidden }) => {
+const CartIcon = ({ toggleCartHidden, itemCount }) => {
 	return (
 		<div className="cart-icon" onClick={toggleCartHidden}>
 			<ShoppingIcon className="shopping-icon " />
-			<span className="item-count">0</span>
+			<span className="item-count">{itemCount}</span>
 		</div>
 	);
 };
 const mapDispatchToProps = (dispatch) => ({
 	toggleCartHidden: () => dispatch(toggleCartHidden())
 });
-export default connect(null, mapDispatchToProps)(CartIcon);
+const mapStateToProps = (state) => ({
+	itemCount: selectCartItemsCount(state)
+});
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
