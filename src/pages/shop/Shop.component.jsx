f9bcf2390abc144903e185b72ab5157946e859ee
Ibrahim Kouma <ikouma269@gmail.com>
2021-08-10 14:23:42 +0800

    refactor our code

--- src/pages/shop/Shop.component.jsx
+++ src/pages/shop/Shop.component.jsx
@@ -1,22 +1,12 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-
-import CollectionPreview from '../../components/collection-preview/collection-preview';
-import { selectCollection } from '../../redux/shop/shop.selector';
+import CollectionsOverview from '../../components/collection-overview/collections-overview.component';
 
 const ShopPage = ({ collections }) => {
 	return (
 		<div className="shop-page">
-			{collections.map(({ id, ...otherCollectionProps }) => (
-				<CollectionPreview key={id} {...otherCollectionProps} />
-			))}
+			<CollectionsOverview />
 		</div>
 	);
 };
 
-const mapStateToProps = () =>
-	createStructuredSelector({
-		collections: selectCollection
-	});
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
---
+++ src/redux/shop/shop.selector.js
@@ -0,0 +1,5 @@
+import { createSelector } from 'reselect';
+
+const selectShop = (state) => state.shop;
+
+export const selectCollection = createSelector([ selectShop ], (shop) => shop.collections);
